cd4ce2dfc8ccc0c7a653d31040a7a78c4a762585
1rlan <irlanidzzze@gmail.com>
2022-10-09 13:40:53 +0300

    homework 2 add

---
+++ ikhabushakhmanovPW2/ikhabushakhmanovPW2/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ikhabushakhmanovPW2
+//
+//  Created by Ирлан Абушахманов on 29.09.2022.
+//
+
+//import UIKit
+//
+//class ViewController: UIViewController {
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//    }
+//}
+
